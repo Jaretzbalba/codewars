ab6ef9d054789ffc21df0b86ba29538c4a0376d8
Jaret Balba <Jaretzbalba@gmail.com>
2022-04-03 09:29:25 -0700

    add 4/3/22 challenges

---
+++ 04_03_2022.js
@@ -0,0 +1,21 @@
+
+//7 kyu: Categorize New Member
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+// input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+// output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+
+function openOrSenior(data){
+    return data.map(arr => (arr[0] >= 55 && arr[1] > 7) ? 'Senior' : 'Open')
+}
+
+// 8 kyu: You only need one - Beginner
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+    return a.includes(x)
+}
