1f36caa1f5665d2579ab6ba89a7d0c199d1504bc
Jaret Balba <Jaretzbalba@gmail.com>
2022-04-05 09:04:55 -0700

    update solution to 4/5/22 challenge

--- 04_05_2022.js
+++ 04_05_2022.js
@@ -31,8 +31,6 @@ function reverseWords(str) {
 const vowels = 'aeiou';
 
 function disemvowel(str) {
-  return str
-    .split('')
-    .filter(letter => !vowels.includes(letter.toLowerCase()))
-    .join('');
+    let vowels = ['a','e','i','o','u'];
+    return str.split('').filter(letter => !vowels.includes(letter.toLowerCase())).join('')
 }
