c0d2ee6f53b8b0c68a0a92a7ba7f50e32b0958d4
Jaret Balba <Jaretzbalba@gmail.com>
2022-04-15 11:34:59 -0700

    add additional 4/15/22 challenges

--- 04_15_2022.js
+++ 04_15_2022.js
@@ -44,3 +44,63 @@ function nbDig(n, d) {
 function isDivideBy(number, a, b) {
     return number % a === 0 && number % b ===0 ? true : false
 }
+
+// 8 kyu: Find the first non-consecutive number
+// Your task is to find the first element of an array that is not consecutive.
+
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+// If the whole array is consecutive then return null2.
+
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; ++i) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+        return arr[i + 1]
+      }
+    }
+    return null
+}
+
+// 8 kyu: Third Angle of a Triangle
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+function otherAngle(a, b) {
+    return 180-(a+b);
+}
+
+// 8 kyu: Sum Mixed Array
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+function sumMix(x){
+    return x.map( a => Number(a)).reduce((a,b) => a + b)
+}
+
+// 8 kyu: Remove exclamation marks
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    return s.split('!').join('')
+}
+
+// 8 kyu: Rock Paper Scissors!
+
+const rps = (p1, p2) => {
+    if((p1 === 'rock' && p2 === 'scissors')||
+      (p1 === 'paper' && p2 === 'rock') ||
+      (p1 === 'scissors' && p2 === 'paper')) {
+      return 'Player 1 won!'
+    } else if (p1 === p2) {
+      return 'Draw!'
+    } else {
+      return 'Player 2 won!'
+    }
+};
