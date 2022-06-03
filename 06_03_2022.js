ca86c0524742e3e9c6d9b4ffc9430cc2411f7e34
Jaret Balba <Jaretzbalba@gmail.com>
2022-06-03 10:50:31 -0700

    add 6/3/22 challenges

--- 06_03_2022.js
+++ 06_03_2022.js
@@ -82,3 +82,42 @@ function dashatize(num) {
     .filter(a => a != '')
     .join('-'));
 }
+
+// 6 kyu: Assemble string
+// In this task, you need to restore a string from a list of its copies.
+
+// You will receive an array of strings. All of them are supposed to be the same as the original but, unfortunately, they were corrupted which means some of the characters were replaced with asterisks ("*").
+
+// You have to restore the original string based on non-corrupted information you have. If in some cases it is not possible to determine what the original character was, use "#" character as a special marker for that.
+
+// If the array is empty, then return an empty string.
+
+// Examples:
+// input = [
+//   "a*cde",
+//   "*bcde",
+//   "abc*e"
+// ]
+// result = "abcde"
+
+// input = [
+//   "a*c**",
+//   "**cd*",
+//   "a*cd*"
+// ]
+// result = "a#cd#"
+
+function assembleString(array) {
+  //if array.length is 0 return ""
+  //if not destructure first element in array and map through that array of strings
+  //declare variable that finds if strings in array have a value of '*'
+  //if that variable returns a value, return that str at that specficied index
+  //if not return '#'
+  //join array
+  return !array.length
+    ? ''
+    : [...array[0]].map((x, i) => {
+        let s = array.find(y => y[i] != '*');
+        return !s ? '#' : s[i];
+      }).join``;
+}
