729f7f75146c2cd0db3db8856b279dee9cd50ccb
Jaret Balba <Jaretzbalba@gmail.com>
2022-12-05 10:14:03 -0800

    add 12/5/22 challenges

---
+++ 12_05_2022.js
@@ -0,0 +1,96 @@
+// 7 kyu: Over The Road
+// Task
+// You've just moved into a perfectly straight street with exactly n identical houses on either side of the road. Naturally, you would like to find out the house number of the people on the other side of the street. The street looks something like this:
+
+// Street
+// 1|   |6
+// 3|   |4
+// 5|   |2
+//   you
+// Evens increase on the right; odds decrease on the left. House numbers start at 1 and increase without gaps. When n = 3, 1 is opposite 6, 3 opposite 4, and 5 opposite 2.
+
+// Example (address, n --> output)
+// Given your house number address and length of street n, give the house number on the opposite side of the street.
+
+// 1, 3 --> 6
+// 3, 3 --> 4
+// 2, 3 --> 5
+// 3, 5 --> 8
+// Note about errors
+// If you are timing out, running out of memory, or get any kind of "error", read on. Both n and address could get upto 500 billion with over 200 random tests. If you try to store the addresses of 500 billion houses in a list then you will run out of memory and the tests will crash. This is not a kata problem so please don't post an issue. Similarly if the tests don't complete within 12 seconds then you also fail.
+
+// To solve this, you need to think of a way to do the kata without making massive lists or huge for loops. Read the discourse for some inspiration :)
+
+function overTheRoad(address, n) {
+  return n * 2 + 1 - address;
+}
+
+// 7 kyu: Currying functions: multiply all elements in an array
+// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+// Example:
+
+// multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+// You must not mutate the original array.
+
+function multiplyAll(arr) {
+  return function multiplier(num) {
+    return arr.map(e => e * num);
+  };
+}
+
+// 6 kyu: Tribonacci Sequence
+// Well met with Fibonacci bigger brother, AKA Tribonacci.
+
+// As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+
+// So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+// [1, 1 ,1, 3, 5, 9, 17, 31, ...]
+// But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+
+// [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+// Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+
+// Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+function tribonacci(signature, n) {
+  let result = [...signature];
+  for (let i = 2; i < n; i++) {
+    result.push(result[i] + result[i - 1] + result[i - 2]);
+  }
+  return result.slice(0, n);
+}
+
+// 6 kyu: Playing with digits
+// Some numbers have funny properties. For example:
+
+// 89 --> 8¹ + 9² = 89 * 1
+
+// 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+// 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+// Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+// we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+// In other words:
+
+// Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be given as strictly positive integers.
+
+// digPow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
+// digPow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
+// digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+// digPow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+function digPow(n, p) {
+  let numArr = n.toString().split('');
+  let result = numArr.reduce((a, b, i) => a + b ** (p + i), 0);
+  return result % n == 0 ? result / n : -1;
+}
