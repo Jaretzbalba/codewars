b0cef09d360bf21b313ce463d56ca2850610639b
Jaret Balba <Jaretzbalba@gmail.com>
2023-01-24 08:40:13 -0800

    add additional 1/24/23 challenges

--- 2023/01_24_2023.js
+++ 2023/01_24_2023.js
@@ -51,3 +51,14 @@ function bouncingBall(h, bounce, window) {
   }
   return numOfBounces;
 }
+
+// 8 kyu: 5 without numbers !!
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+function unusualFive() {
+  return 'aaaaa'.length;
+}
