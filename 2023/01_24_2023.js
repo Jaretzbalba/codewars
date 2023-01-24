785ec5e72c0350512471e9c001fd8d66bbb2d35e
Jaret Balba <Jaretzbalba@gmail.com>
2023-01-24 08:20:46 -0800

    add 1/24/23 challenges

---
+++ 2023/01_24_2023.js
@@ -0,0 +1,17 @@
+// 7 kyu: Vowel Count
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  return str.match(/[aeiou]/gi).length;
+}
+
+//OR
+
+function getCount(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  return str.split('').filter(e => vowels.includes(e)).length;
+}
