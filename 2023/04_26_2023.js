aa317079483c7611607831d3775d27d9a30c9bff
Jaret Balba <Jaretzbalba@gmail.com>
2023-04-26 08:28:24 -0700

    add 4/26/23 challenges

---
+++ 2023/04_26_2023.js
@@ -0,0 +1,104 @@
+// 7 kyu: Printer Errors
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+// Examples:
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+function printerError(s) {
+  //initialze a count for letters not within range a-m
+  //split string and iterate through each letter
+  //if letter isn't within range, add to count
+  //return count / string.length
+  let errors = 0
+  let countErrors = s
+    .split('')
+    .forEach(letter => letter.charCodeAt(0) > 109 && errors++)
+  return `${errors}/${s.length}`
+}
+
+// 7 kyu: Ones and Zeros
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''), 2)
+}
+
+// 7 kyu: Number of People in the Bus
+// There is a bus moving in the city which takes and drops some people at each bus stop.
+
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+// The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+
+var number = function (busStops) {
+  return busStops.reduce((sum, stop) => {
+    return sum + stop[0] - stop[1]
+  }, 0)
+}
+
+//OR
+
+const number = busStops =>
+  busStops.reduce((rem, [on, off]) => rem + on - off, 0)
+
+// 6 kyu: Take a Ten Minutes Walk
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+function isValidWalk(walk) {
+  //if length of walk array isn't 10 return false
+  //if # of n/s and # of e/w are equal return true, otherwise return false
+  let dy = 0,
+    dx = 0,
+    dt = walk.length
+  walk.forEach(e => {
+    switch (e) {
+      case 'n':
+        dy++
+        break
+      case 's':
+        dy--
+        break
+      case 'e':
+        dx++
+        break
+      case 'w':
+        dx--
+        break
+    }
+  })
+  return dt == 10 && dy == 0 && dx == 0
+}
