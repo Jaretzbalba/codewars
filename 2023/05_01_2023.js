230fc6e1cdf6bb7500bfd394172275a838503799
Jaret Balba <Jaretzbalba@gmail.com>
2023-05-01 08:24:49 -0700

    add 5/1/23 challenges

---
+++ 2023/05_01_2023.js
@@ -0,0 +1,55 @@
+// 7 kyu: The highest profit wins!
+// Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+// Examples (Input --> Output)
+// [1,2,3,4,5] --> [1,5]
+// [2334454,5] --> [5,2334454]
+// [1]         --> [1,1]
+
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)]
+}
+
+// 7 kyu: Remove the minimum
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+function removeSmallest(numbers) {
+  let nums = numbers.slice(0)
+  let index = numbers.indexOf(Math.min(...numbers))
+  nums.splice(index, 1)
+  return nums
+}
+
+// 7 kyu: Count the divisors of a number
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// Examples (input --> output)
+// 4 --> 3 // we have 3 divisors - 1, 2 and 4
+// 5 --> 2 // we have 2 divisors - 1 and 5
+// 12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+// 30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+// Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+function getDivisorsCnt(n) {
+  //initialize count
+  let count = 0
+  let num = n
+  while (num > 0) {
+    if (n % num === 0) {
+      count++
+    }
+    num--
+  }
+}
