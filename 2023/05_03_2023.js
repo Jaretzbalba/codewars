42a6837ce37e3b0f24792a5bf0580ac7fc3a4437
Jaret Balba <Jaretzbalba@gmail.com>
2023-05-03 09:07:13 -0700

    add 5/3/23 challenges

---
+++ 2023/05_03_2023.js
@@ -0,0 +1,84 @@
+// 7 kyu: Make a function that does arithmetic!
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+// The four operators are "add", "subtract", "divide", "multiply".
+
+// A few examples:(Input1, Input2, Input3 --> Output)
+
+// 5, 2, "add"      --> 7
+// 5, 2, "subtract" --> 3
+// 5, 2, "multiply" --> 10
+// 5, 2, "divide"   --> 2.5
+
+function arithmetic(a, b, operator) {
+  switch (operator) {
+    case 'add':
+      return a + b
+    case 'subtract':
+      return a - b
+    case 'multiply':
+      return a * b
+    case 'divide':
+      return a / b
+  }
+}
+
+// 7 kyu: Count the Digit
+// Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+// Square all numbers k (0 <= k <= n) between 0 and n.
+
+// Count the numbers of digits d used in the writing of all the k**2.
+
+// Implement the function taking n and d as parameters and returning this count.
+
+// Examples:
+// n = 10, d = 1
+// the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+// We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+// The function, when given n = 25 and d = 1 as argument, should return 11 since
+// the k*k that contain the digit 1 are:
+// 1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+// So there are 11 digits 1 for the squares of numbers between 0 and 25.
+// Note that 121 has twice the digit 1.
+
+function nbDig(n, d) {
+  let numArr = [...Array(n + 1).keys()]
+  let allDigits = numArr.map(e => (e ** 2).toString().split('')).flat()
+  return allDigits.filter(e => e == d).length
+}
+
+// LC Easy: 2619. Array Prototype Last
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: 3
+// Explanation: Calling nums.last() should return the last element: 3.
+
+Array.prototype.last = function () {
+  return this.length > 0 ? this[this.length - 1] : -1
+}
+
+// LC Medium: 27. Remove Element
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+var removeElement = function (nums, val) {
+  //iterate through nums array
+  //if nums = val, then pop
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+      i--
+    }
+  }
+}
